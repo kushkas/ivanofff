74070ba22f91ebbcf0f2cd18fe68ec5ff9c6722f
kushkas <90375206+kushkas@users.noreply.github.com>
2022-09-14 20:34:49 +0300

    Add files via upload

---
+++ front/src/components/navbar.vue
@@ -0,0 +1,54 @@
+<template>
+    <div>
+        <v-app-bar
+                color="blue accent-4"
+                dense
+                dark
+        >
+
+            <v-toolbar-title>Спорт товары</v-toolbar-title>
+
+
+            <v-btn
+                    elevation="2"
+                    outlined
+                    class="mr-3 ml-5"
+            >
+                <router-link to="/">Товары</router-link>
+
+            </v-btn>
+            <v-btn
+                    elevation="2"
+                    outlined
+                    class="mr-3"
+            >
+                <router-link to="/brands">Брэнды</router-link>
+            </v-btn>
+            <v-btn
+                    elevation="2"
+                    color="blue-grey"
+                    @click="clearFilter"
+                    v-if="$store.getters.getFilterGoods != -1"
+            >
+                Отчистить фильтр
+            </v-btn>
+
+        </v-app-bar>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'nav-bar',
+        methods: {
+            clearFilter(){
+                this.$store.commit("emitFilter",-1)
+                this.$store.dispatch("getGoodsAction");
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
